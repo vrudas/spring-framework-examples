2ae7c9d56590cf396adb9ca0f71f742517fb13c8
vrudas <vrudas4work@gmail.com>
2021-01-22 00:24:27 +0200

    Implemented hello world module

---
+++ example-00-hello/src/main/java/io/sfexamples/hello/Main.java
@@ -0,0 +1,18 @@
+package io.sfexamples.hello;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        var applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        var noteByName = (Note) applicationContext.getBean("helloWorldNote");
+        System.out.println("noteByName = " + noteByName);
+
+        var noteByType = applicationContext.getBean(Note.class);
+        System.out.println("noteByType = " + noteByType);
+
+        var noteByNameAndType = applicationContext.getBean("helloWorldNote", Note.class);
+        System.out.println("noteByNameAndType = " + noteByNameAndType);
+    }
+}
