998bf9932b9feebac8bca226f72cfeddb79900c0
vrudas <vrudas4work@gmail.com>
2021-01-23 13:41:12 +0200

    Added custom bean factory example

---
+++ example-01-bean-factory/src/main/java/io/sfexamples/beanfactory/Main.java
@@ -0,0 +1,24 @@
+package io.sfexamples.beanfactory;
+
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+
+public class Main {
+    public static void main(String[] args) {
+        var applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        DefaultListableBeanFactory listableBeanFactory = new DefaultListableBeanFactory();
+        XmlBeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(listableBeanFactory);
+        beanDefinitionReader.loadBeanDefinitions(new ClassPathResource("applicationContext.xml"));
+
+        var noteFromApplicationContext = applicationContext.getBean("helloWorldNote", Note.class);
+        var noteFromCustomBeanFactory = listableBeanFactory.getBean("helloWorldNote", Note.class);
+
+        System.out.println("noteFromApplicationContext = " + noteFromApplicationContext);
+        System.out.println("noteFromCustomBeanFactory = " + noteFromCustomBeanFactory);
+
+        System.out.println("Beans are not the same: " + (noteFromApplicationContext != noteFromCustomBeanFactory));
+    }
+}
---
+++ example-01-bean-factory/src/main/java/io/sfexamples/beanfactory/Note.java
@@ -0,0 +1,36 @@
+package io.sfexamples.beanfactory;
+
+import java.util.Objects;
+
+public class Note {
+
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Note note = (Note) o;
+        return Objects.equals(text, note.text);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(text);
+    }
+
+    @Override
+    public String toString() {
+        return "Note{" +
+            "text='" + text + '\'' +
+            '}';
+    }
+}
