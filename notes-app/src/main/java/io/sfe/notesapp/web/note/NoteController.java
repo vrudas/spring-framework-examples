11b660245827a6411e1e15f613521be09bd9e65f
vrudas <vrudas4work@gmail.com>
2021-02-01 00:32:43 +0200

    Organized templates for note entity

--- notes-app/src/main/java/io/sfe/notesapp/web/note/NoteController.java
+++ notes-app/src/main/java/io/sfe/notesapp/web/note/NoteController.java
@@ -1,16 +1,24 @@
 package io.sfe.notesapp.web.note;
 
 import io.sfe.notesapp.domain.note.Note;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/notes")
 public class NoteController {
 
-    @RequestMapping("/notes")
+    @RequestMapping(
+        path = {"", "/"},
+        method = RequestMethod.GET,
+        produces = MediaType.TEXT_HTML_VALUE,
+        consumes = MediaType.ALL_VALUE
+    )
     public String allNotes(Model model) {
         model.addAttribute(
             "notes",
@@ -21,6 +29,6 @@ public class NoteController {
             )
         );
 
-        return "notes";
+        return "note/notes";
     }
 }
