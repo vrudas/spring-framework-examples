97367b3dacbb2802ac8fe2206c9fcbcec108ebe6
vrudas <vrudas4work@gmail.com>
2021-02-06 23:00:32 +0200

    Implemented delete note Data test

--- notes-app/src/test/java/io/sfe/notesapp/storage/note/NoteRepositoryTest.java
+++ notes-app/src/test/java/io/sfe/notesapp/storage/note/NoteRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 @DataJdbcTest
 public class NoteRepositoryTest {
@@ -24,6 +25,27 @@ public class NoteRepositoryTest {
     public void no_history_records_in_db() {
         long notesCount = noteRepository.count();
         assertThat(notesCount).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("Nothing happened when trying to delete not existing note")
+    public void nothing_happened_when_trying_to_delete_not_existing_note() {
+        assertThatCode(() -> noteRepository.deleteById(1))
+            .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("Note was deleted")
+    public void note_was_deleted() {
+        var noteToSave = NoteEntity.of("text");
+
+        var savedNoteEntity = noteRepository.save(noteToSave);
+
+        assertThat(noteRepository.count()).isEqualTo(1);
+
+        noteRepository.delete(savedNoteEntity);
+
+        assertThat(noteRepository.count()).isEqualTo(0);
     }
 
 }
