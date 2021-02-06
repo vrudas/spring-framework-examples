63f3dd75100c222d991c5a8fff4e5a50a1aed372
vrudas <vrudas4work@gmail.com>
2021-02-06 19:25:22 +0200

    Added Note row mapper

--- notes-app/src/main/java/io/sfe/notesapp/storage/note/NoteJdbcTemplateRepository.java
+++ notes-app/src/main/java/io/sfe/notesapp/storage/note/NoteJdbcTemplateRepository.java
@@ -1,5 +1,6 @@
 package io.sfe.notesapp.storage.note;
 
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Component;
@@ -8,6 +9,12 @@ import java.util.Map;
 
 @Component
 public class NoteJdbcTemplateRepository {
+
+    private static final RowMapper<NoteEntity> NOTE_ENTITY_ROW_MAPPER = (rs, rowNum) -> {
+        var id = rs.getInt("id");
+        var text = rs.getString("text");
+        return NoteEntity.of(text).withId(id);
+    };
 
     private final NamedParameterJdbcTemplate namedJdbcTemplate;
 
