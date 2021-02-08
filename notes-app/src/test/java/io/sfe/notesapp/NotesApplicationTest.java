651f95c7a1dbfecef0c6458ee097a1a6f4399d2b
vrudas <vrudas4work@gmail.com>
2021-02-08 23:33:39 +0200

    Implemented context startup test

--- notes-app/src/test/java/io/sfe/notesapp/NotesApplicationTest.java
+++ notes-app/src/test/java/io/sfe/notesapp/NotesApplicationTest.java
@@ -2,7 +2,9 @@ package io.sfe.notesapp;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class NotesApplicationTest {
 
     @Test
