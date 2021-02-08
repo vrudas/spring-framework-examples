9ef181a964408c89b057668a8b2d62b08c89b1a6
vrudas <vrudas4work@gmail.com>
2021-02-09 00:43:55 +0200

    Implemented TestPropertySourcesTest example

---
+++ notes-app/src/test/java/io/sfe/notesapp/TestPropertySourcesTest.java
@@ -0,0 +1,32 @@
+package io.sfe.notesapp;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration
+@TestPropertySource(
+    locations = "/test.properties",
+    properties = {"application.port: 4242"}
+)
+public class TestPropertySourcesTest {
+
+    @Value("${application.name}")
+    private String applicationName;
+
+    @Value("${application.port}")
+    private int applicationPort;
+
+    @Test
+    void verify_properties() {
+        assertThat(applicationName).isEqualTo("notes-app-test");
+        assertThat(applicationPort).isEqualTo(4242);
+    }
+}
