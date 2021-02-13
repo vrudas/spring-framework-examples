02bc5327294d7bb1499eeb3badb6a7b2e5a717ef
vrudas <vrudas4work@gmail.com>
2021-02-13 22:41:17 +0200

    Implemented InMemorySecurity example

---
+++ example-13-inmemory-security/src/main/java/io/sfe/bootsecurity/InMemorySecurityConfig.java
@@ -0,0 +1,32 @@
+package io.sfe.bootsecurity;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+
+@Configuration
+public class InMemorySecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .passwordEncoder(new Pbkdf2PasswordEncoder())
+            .withUser(
+                User.builder()
+                    .username("user")
+                    .password("bacecc4236b50d27c7e580d6f7ec844251cdbec028b545154b3734fa63075b82017fb649be13fd17")
+                    .roles("USER")
+                    .build()
+            );
+
+        auth.inMemoryAuthentication()
+            .passwordEncoder(new BCryptPasswordEncoder())
+            .withUser("admin")
+            .password("$2a$10$kF9qWGfBqKqqO9PuG/XLZuuPq601zbtV3F4v8.mYVX0ilBsvbjjpW")
+            .roles("ADMIN");
+    }
+
+}
---
+++ example-13-inmemory-security/src/main/java/io/sfe/bootsecurity/SecuredNoteApplication.java
@@ -0,0 +1,13 @@
+package io.sfe.bootsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuredNoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecuredNoteApplication.class, args);
+    }
+
+}
