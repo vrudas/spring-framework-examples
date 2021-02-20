b1215fb1f6895522a714fb9598604edb44fec459
vrudas <vrudas4work@gmail.com>
2021-02-20 22:47:54 +0200

    Added oauth example module

---
+++ example-20-oauth/src/main/java/io/sfe/oauth/OAuthApplication.java
@@ -0,0 +1,13 @@
+package io.sfe.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAuthApplication.class, args);
+    }
+
+}
