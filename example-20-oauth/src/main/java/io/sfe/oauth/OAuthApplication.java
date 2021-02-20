3120e41f3746111703664da1791bde0e6d211a0b
vrudas <vrudas4work@gmail.com>
2021-02-20 23:06:18 +0200

    Added secrets property source

--- example-20-oauth/src/main/java/io/sfe/oauth/OAuthApplication.java
+++ example-20-oauth/src/main/java/io/sfe/oauth/OAuthApplication.java
@@ -2,8 +2,10 @@ package io.sfe.oauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:secrets/secrets.properties")
 public class OAuthApplication {
 
     public static void main(String[] args) {
