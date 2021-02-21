f827fa26f3602ed146c8b68d1b6a324db69ba2fe
vrudas <vrudas4work@gmail.com>
2021-02-21 23:13:22 +0200

    Added JwtTokenFilter

--- example-21-jwt/src/main/java/io/sfe/jwt/security/SecurityConfig.java
+++ example-21-jwt/src/main/java/io/sfe/jwt/security/SecurityConfig.java
@@ -1,6 +1,8 @@
 package io.sfe.jwt.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -24,6 +26,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
 }
