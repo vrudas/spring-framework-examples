dd9af118d33425243ef8df2a7b318db96c7fa680
vrudas <vrudas4work@gmail.com>
2021-04-16 19:02:00 +0300

    Add profiles example

--- notes-app/src/main/java/io/sfe/notesapp/web/IndexController.java
+++ notes-app/src/main/java/io/sfe/notesapp/web/IndexController.java
@@ -1,13 +1,22 @@
 package io.sfe.notesapp.web;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
+    private final String applicationName;
+
+    public IndexController(@Value("${spring.application.name}") String applicationName) {
+        this.applicationName = applicationName;
+    }
+
     @GetMapping({"", "/", "/index"})
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("applicationName", applicationName);
         return "index";
     }
 }
