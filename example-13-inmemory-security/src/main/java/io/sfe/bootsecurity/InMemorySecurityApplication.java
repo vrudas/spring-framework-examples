e603d3a0c32189aff6d7d54ab0b99fa077ec66ec
vrudas <vrudas4work@gmail.com>
2023-03-09 00:17:02 +0200

    Updated InMemory security example to latest Spring Security version

--- example-13-inmemory-security/src/main/java/io/sfe/bootsecurity/InMemorySecurityApplication.java
+++ example-13-inmemory-security/src/main/java/io/sfe/bootsecurity/InMemorySecurityApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecuredNoteApplication {
+public class InMemorySecurityApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SecuredNoteApplication.class, args);
+        SpringApplication.run(InMemorySecurityApplication.class, args);
     }
 
 }
--- example-13-inmemory-security/src/main/java/io/sfe/bootsecurity/InMemorySecurityConfig.java
+++ example-13-inmemory-security/src/main/java/io/sfe/bootsecurity/InMemorySecurityConfig.java
@@ -1,42 +1,47 @@
 package io.sfe.bootsecurity;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class InMemorySecurityConfig extends WebSecurityConfigurerAdapter {
+public class InMemorySecurityConfig {
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-            .passwordEncoder(new Pbkdf2PasswordEncoder())
-            .withUser(
-                User.builder()
-                    .username("user")
-                    .password("bacecc4236b50d27c7e580d6f7ec844251cdbec028b545154b3734fa63075b82017fb649be13fd17")
-                    .roles("USER")
-                    .build()
-            );
-
-        auth.inMemoryAuthentication()
-            .passwordEncoder(new BCryptPasswordEncoder())
-            .withUser("admin")
-            .password("$2a$10$kF9qWGfBqKqqO9PuG/XLZuuPq601zbtV3F4v8.mYVX0ilBsvbjjpW")
-            .roles("ADMIN");
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
-    // HTTP Basic Example
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-            .mvcMatchers("/").permitAll()
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        var user = User.withUsername("user")
+            .password("18b146cac8026852b8b9ec406bc84ed2d334a3dc06") //user
+            .roles("USER")
+            .build();
+
+        var admin = User.withUsername("admin")
+            .password("{bcrypt}$2a$10$ku.DZ5JqOy/dgFgAZkwcSuiaMMCmOt8pVmerZDM5lTWO44MHGCMcC") //admin
+            .roles("ADMIN")
+            .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    /**
+     * HTTP Basic Example
+     */
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests()
+            .requestMatchers("/").permitAll()
             .anyRequest().authenticated()
             .and()
-            .httpBasic();
+            .httpBasic()
+            .and().build();
     }
 }
