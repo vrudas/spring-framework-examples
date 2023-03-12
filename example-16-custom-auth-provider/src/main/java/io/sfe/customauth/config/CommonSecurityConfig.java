d42db0955b0a56776e42c660b311effa867a4bf6
vrudas <vrudas4work@gmail.com>
2023-03-13 00:30:49 +0200

    Used DelegatingPasswordEncoder

--- example-16-custom-auth-provider/src/main/java/io/sfe/customauth/config/CommonSecurityConfig.java
+++ example-16-custom-auth-provider/src/main/java/io/sfe/customauth/config/CommonSecurityConfig.java
@@ -3,7 +3,7 @@ package io.sfe.customauth.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
@@ -31,6 +31,6 @@ public class CommonSecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
