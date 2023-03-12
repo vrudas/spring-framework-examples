2e8218f4d7b6f7d3ae8cf979d33955c8eb503c5b
vrudas <vrudas4work@gmail.com>
2023-03-13 00:31:05 +0200

    Migrated SecurityConfig to Spring Security 6

--- example-16-custom-auth-provider/src/main/java/io/sfe/customauth/config/SecurityConfig.java
+++ example-16-custom-auth-provider/src/main/java/io/sfe/customauth/config/SecurityConfig.java
@@ -2,44 +2,27 @@ package io.sfe.customauth.config;
 
 import io.sfe.customauth.auth.LoginPasswordAuthenticationProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import javax.sql.DataSource;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    private final DataSource dataSource;
-    private final PasswordEncoder passwordEncoder;
-
-    public SecurityConfig(DataSource dataSource, PasswordEncoder passwordEncoder) {
-        this.dataSource = dataSource;
-        this.passwordEncoder = passwordEncoder;
-    }
+public class SecurityConfig {
 
     @Autowired
-    void configureGlobal(
+    public void configureAuthenticationManager(
         AuthenticationManagerBuilder auth,
         LoginPasswordAuthenticationProvider loginPasswordAuthenticationProvider
-    ) throws Exception {
-        auth.authenticationProvider(loginPasswordAuthenticationProvider)
-            .jdbcAuthentication()
-            .passwordEncoder(passwordEncoder)
-            .dataSource(dataSource)
-            .withDefaultSchema()
-            .withUser("admin")
-            .password("$2a$10$kF9qWGfBqKqqO9PuG/XLZuuPq601zbtV3F4v8.mYVX0ilBsvbjjpW")
-            .roles("ADMIN");
+    ) {
+        auth.authenticationProvider(loginPasswordAuthenticationProvider);
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeRequests()
+            .authorizeHttpRequests()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
@@ -52,6 +35,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutUrl("/logout")
                 .logoutSuccessUrl("/login?logout")
                 .permitAll();
+
+        return http.build();
     }
 
 }
