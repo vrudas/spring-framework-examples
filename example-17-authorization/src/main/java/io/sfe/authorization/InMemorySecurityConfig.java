0650d490e4189d093cadda41e950ba4fcf96bc13
vrudas <vrudas4work@gmail.com>
2023-03-15 00:37:43 +0200

    Migrated authorization example to Spring Security 6

--- example-17-authorization/src/main/java/io/sfe/authorization/InMemorySecurityConfig.java
+++ example-17-authorization/src/main/java/io/sfe/authorization/InMemorySecurityConfig.java
@@ -1,52 +1,65 @@
 package io.sfe.authorization;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 
 @Configuration
-public class InMemorySecurityConfig extends WebSecurityConfigurerAdapter {
+public class InMemorySecurityConfig {
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Autowired
+    void configureAuthenticationManager(
+        AuthenticationManagerBuilder auth
+    ) throws Exception {
         auth.inMemoryAuthentication()
-            .passwordEncoder(new Pbkdf2PasswordEncoder())
             .withUser(
                 User.builder()
                     .username("user")
-                    .password("bacecc4236b50d27c7e580d6f7ec844251cdbec028b545154b3734fa63075b82017fb649be13fd17")
+                    .password("{bcrypt}$2a$10$GlpFG1Ml3U9AvkOu0D1B9ufnoquX5xqCR/NHaMfBZliYgPa8/e5sK")
                     .roles("USER")
                     .authorities(new SimpleGrantedAuthority("READ_USERS"))
                     .build()
             );
 
         auth.inMemoryAuthentication()
-            .passwordEncoder(new BCryptPasswordEncoder())
             .withUser("admin")
-            .password("$2a$10$kF9qWGfBqKqqO9PuG/XLZuuPq601zbtV3F4v8.mYVX0ilBsvbjjpW")
+            .password("{bcrypt}$2a$10$kF9qWGfBqKqqO9PuG/XLZuuPq601zbtV3F4v8.mYVX0ilBsvbjjpW")
             .authorities("DELETE_USERS")
             .roles("USER", "ADMIN");
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-            .mvcMatchers("/login").permitAll()
-            .mvcMatchers("/admin/**").hasRole("ADMIN")
-            .mvcMatchers("/read-user").hasAnyAuthority("READ_USERS")
-            .mvcMatchers("/delete-user").access("hasRole('ADMIN') and hasAuthority('DELETE_USERS')")
-            .anyRequest().authenticated()
-            .and()
-            .formLogin()
-        ;
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+        HttpSecurity http
+    ) throws Exception {
+        http
+            .authorizeHttpRequests(authRegistry -> authRegistry
+                .requestMatchers("/login").permitAll()
+                .requestMatchers("/read-user/**").hasAnyAuthority("READ_USERS")
+                .requestMatchers("/admin/**").hasRole("ADMIN")
+                .requestMatchers("/delete-user/**").access(
+                    new WebExpressionAuthorizationManager("hasRole('ADMIN') and hasAuthority('DELETE_USERS')")
+                )
+                .anyRequest().authenticated()
+            )
+            .formLogin();
+
+        return http.build();
     }
 
     @Bean
