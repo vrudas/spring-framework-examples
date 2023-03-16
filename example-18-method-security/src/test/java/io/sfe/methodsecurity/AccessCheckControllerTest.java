f9c7c0bf360a636d20fd4e55c7934e9f4ceb2d1b
vrudas <vrudas4work@gmail.com>
2023-03-16 20:55:20 +0200

    Fixed tests by importing security configuration class
    Recommendation from https://docs.spring.io/spring-boot/docs/3.0.x/reference/html/howto.html#howto.testing.slice-tests

--- example-18-method-security/src/test/java/io/sfe/methodsecurity/AccessCheckControllerTest.java
+++ example-18-method-security/src/test/java/io/sfe/methodsecurity/AccessCheckControllerTest.java
@@ -3,12 +3,14 @@ package io.sfe.methodsecurity;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Import(InMemorySecurityConfig.class)
 @WebMvcTest(AccessCheckController.class)
 class AccessCheckControllerTest {
 
